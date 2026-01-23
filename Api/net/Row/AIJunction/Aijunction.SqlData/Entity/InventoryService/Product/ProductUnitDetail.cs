d6484bf0ff06758ef13be4db2470ca466573e3ef
kmrjyotigit <kmrjyotigit@gmail.com>
2026-01-23 12:39:11 +0530

    chore: ignore nx workspace data

--- Api/net/Row/AIJunction/Aijunction.SqlData/Entity/InventoryService/Product/ProductUnitDetail.cs
+++ Api/net/Row/AIJunction/Aijunction.SqlData/Entity/InventoryService/Product/ProductUnitDetail.cs
@@ -1,25 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using AvinyaCrm.SqlData.Domain.Entities.CommonService;
+using Aijunction.SqlData.Entity.CommonService;
 using Microsoft.EntityFrameworkCore;
 
-namespace AvinyaCrm.SqlData.Domain.Entities.InventoryService;
+namespace Aijunction.SqlData.Entity.InventoryService.Product;
 [Table("ProductUnitDetail", Schema = "INV.MS")]
 public class ProductUnitEntity : MasterBaseEntity
 {
     [Key]
-    [Column("product_unit_id")]
+    [Column("product_unit_global_id")]
     [MaxLength(36)]
     [Unicode(false)]
-    public string ProductUnitId { get; set; } = Guid.NewGuid().ToString();
-    [Column("product_uniq_id")]
+    public string ProductUnitGlobalId { get; set; } = Guid.NewGuid().ToString();
+    [Column("product_global_id")]
     [MaxLength(50)]
     [Unicode(false)]
-    public string? ProductUniqId { get; set; }
+    public string? ProductGlobalId { get; set; }
     [Required]
-    [Column("unit_unique_id")]
+    [Column("unit_global_id")]
     [MaxLength(50)]
-    public string UnitUniqueId { get; set; } = null!;
+    public string UnitGlobalId { get; set; } = null!;
     [Column("is_primary_unit")]
     [MaxLength(50)]
     public string IsPrimaryUnit { get; set; } = null!;
